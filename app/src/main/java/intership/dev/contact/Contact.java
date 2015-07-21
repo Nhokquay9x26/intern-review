b89560618d14b440e036e5dff05941c00fee6b05
Dao Quoc Viet <daoquocviet11i3@gmail.com>
2015-07-21 10:32:45 +0700

    Create class Contact

---
+++ app/src/main/java/intership/dev/contact/Contact.java
@@ -0,0 +1,25 @@
+package intership.dev.contact;
+
+/**
+ * Created by nhokquay9x26 on 7/20/15.
+ */
+public class Contact {
+    private int mImgContacts;
+    private String mNameContacts;
+
+    public int getmImgContacts() {
+        return mImgContacts;
+    }
+
+    public void setmImgContacts(int mImgContacts) {
+        this.mImgContacts = mImgContacts;
+    }
+
+    public String getmNameContacts() {
+        return mNameContacts;
+    }
+
+    public void setmNameContacts(String mNameContacts) {
+        this.mNameContacts = mNameContacts;
+    }
+}
